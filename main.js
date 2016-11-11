ee354b3ecce5553305ca698fdf58f1072a974f7b
marinn17 <cedillo.marinn@gmail.com>
2016-11-11 15:28:45 -0800

    finallysdfhkjs

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
- // Initialize Firebase
-  var config = {
+ /*global firebase*/
+ /*global $*/
+ var config = {
     apiKey: "AIzaSyC4sa5bUPRkMCdrEbZ5x8G_lg5uLuLwroQ",
     authDomain: "chat-app-4d538.firebaseapp.com",
     databaseURL: "https://chat-app-4d538.firebaseio.com",
     storageBucket: "chat-app-4d538.appspot.com",
     messagingSenderId: "895862420876"
-  };
-<<<<<<< HEAD
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var chatData = firebase.database().ref();
-=======
-  firebase.initializeApp(config);
->>>>>>> origin/gh-pages
+var chatData = firebase.database().ref();
+
 function pushMessage(event) {
   if (event.keyCode == 13) {
     var name = $('#nameInput').val();
@@ -20,4 +18,22 @@ function pushMessage(event) {
     chatData.push({name: name, text: text});
     $('#messageInput').val('');
   }
+}
+
+$('#messageInput').keypress(pushMessage);
+
+chatData.on("child_added", showMessage);
+
+function showMessage(msg) {
+  var message = msg.val();
+  var messageSender = message.name;
+  var messageContent = message.text;
+
+  var messageEl = $("<div/>").addClass("message");
+  var senderEl = $("<span/>").text(messageSender + ": ");
+  var contentEl = $("<span/>").text(messageContent);
+  
+  messageEl.append(senderEl);
+  messageEl.append(contentEl);
+  $('#messages').append(messageEl);
 }
